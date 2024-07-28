cbcda017f4d2125cb8aae168ac7797132ac43c33
Brayan Yepes <abrayan.claye@gmail.com>
2024-07-28 18:16:10 -0500

    mejorar boton de inputsearch

--- src/frontend/components/molecules/InputSearch/InputSearch.tsx
+++ src/frontend/components/molecules/InputSearch/InputSearch.tsx
@@ -8,7 +8,7 @@ export const InputSearch = ({ setListProductsFiltered }: InputSearchProps) => {
     return (
         <div className='w-full my-4 flex justify-center items-center gap-4'>
             <input onChange={handleInputChange} type="text" className='w-full bg-gray-100 border border-gray-300 rounded p-2 outline-none focus:border focus:border-blue-500'/>
-            <button className='bg-blue-500 rounded p-2' onClick={handleSearchClick}>
+            <button className='bg-blue-500 hover:bg-blue-500 text-white rounded p-2' onClick={handleSearchClick}>
                 Buscar
             </button>
         </div>
